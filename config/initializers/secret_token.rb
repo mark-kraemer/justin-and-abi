0304c58f50cf73e49ea8c5888099901a088ab516
Mark Kraemer <mark.kraemer@collegeplus.org>
2013-11-22 19:28:30 -0600

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+JustinAndAbi::Application.config.secret_key_base = '0950c946883181e7e3cc4595887eed1d632981b55f6856a1653d17162d67e79eb61b8ed29e55bc407c4fd3f0bcaa5ef71265a9d936206ffa6d4c48a47c2b4749'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+JustinAndAbi::Application.config.session_store :cookie_store, key: '_justin-and-abi_session'
