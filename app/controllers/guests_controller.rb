1df3b59c0c9ab7853c14b243fe84d6cf32028994
Mark Kraemer <mark.kraemer@collegeplus.org>
2013-11-23 09:27:34 -0600

    Basic form save

--- app/controllers/guests_controller.rb
+++ app/controllers/guests_controller.rb
@@ -4,4 +4,18 @@ class GuestsController < ApplicationController
     @guest = Guest.new
   end
 
+  def create
+    @guest = Guest.new guest_params
+    if @guest.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def guest_params
+    params.require(:guest).permit :name, :email, :head_count
+  end
 end
