5b8254ba2144be83df47f575d9826a6af4ea10b3
Mark Kraemer <mark.kraemer@collegeplus.org>
2013-11-22 20:29:26 -0600

    Generate guest model

---
+++ db/migrate/20131123022720_create_guests.rb
@@ -0,0 +1,15 @@
+class CreateGuests < ActiveRecord::Migration
+  def change
+    create_table :guests do |t|
+      t.string :name
+      t.string :email
+      t.string :accommodations
+      t.boolean :attending
+      t.text :comments
+      t.integer :head_count
+
+      t.timestamps
+    end
+    add_index :guests, :attending
+  end
+end
